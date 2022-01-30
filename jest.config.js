2fc871a0e661a057ef1850074832ba090c504a8d
Phillip Clark <phillip@flitbit.com>
2022-01-30 09:32:06 -0700

    reworked ts, building esm, cjs, and browser

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(src/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
+  // coverageThreshold: {
+  //   global: {
+  //     // global thresholds
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
+  coverageReporters: ['lcov', 'text'],
+};
--- src/cleanup.spec.ts
+++ src/cleanup.spec.ts
@@ -1,36 +1,60 @@
-import { expect } from 'chai';
 import { EventEmitter } from 'events';
-import { cleanupPropagationEvent, addCleanupTask } from '../src';
+import { cleanupPropagationEvent, addCleanupTask, Listener, Task } from '.';
 
 const delay = async (ms: number): Promise<void> => {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 };
 
+const DummyListener = (): void => undefined;
+
 describe('.cleanupPropagationEvent(sender, listener, target, event, reciprocal?)', () => {
   it('throws when sender not specified', () => {
-    expect(() => cleanupPropagationEvent(undefined, undefined, undefined, undefined)).to.throw(
-      'sender (object) is required'
-    );
+    expect(() =>
+      cleanupPropagationEvent(
+        undefined as unknown as EventEmitter,
+        undefined as unknown as string,
+        undefined as unknown as Listener,
+        undefined as unknown as EventEmitter,
+      ),
+    ).toThrow('sender (object) is required');
   });
   it('throws when event not specified', () => {
     const sender = new EventEmitter();
-    expect(() => cleanupPropagationEvent(sender, undefined, undefined, undefined)).to.throw(
-      'event (string) is required'
-    );
+    expect(() =>
+      cleanupPropagationEvent(
+        sender,
+        undefined as unknown as string,
+        undefined as unknown as Listener,
+        undefined as unknown as EventEmitter,
+      ),
+    ).toThrow('event (string) is required');
   });
 
   it('throws when listener not specified', () => {
     const sender = new EventEmitter();
     const event = 'close';
-    expect(() => cleanupPropagationEvent(sender, event, undefined, undefined)).to.throw('listener (func) is required');
+    expect(() =>
+      cleanupPropagationEvent(
+        sender,
+        event,
+        undefined as unknown as Listener,
+        undefined as unknown as EventEmitter,
+      ),
+    ).toThrow('listener (func) is required');
   });
   it('throws when target not specified', () => {
     const sender = new EventEmitter();
     const event = 'close';
-    const listener = (): void => null;
-    expect(() => cleanupPropagationEvent(sender, event, listener, undefined)).to.throw('target (object) is required');
+    expect(() =>
+      cleanupPropagationEvent(
+        sender,
+        event,
+        DummyListener,
+        undefined as unknown as EventEmitter,
+      ),
+    ).toThrow('target (object) is required');
   });
   it('succeeds when required args specified', () => {
     const observations: string[] = [];
@@ -52,7 +76,7 @@ describe('.cleanupPropagationEvent(sender, listener, target, event, reciprocal?)
     const event = 'close';
     cleanupPropagationEvent(sender, event, listener, target);
     sender.emit(event);
-    expect(observations).to.contain('listener');
+    expect(observations).toContain('listener');
   });
   it('reciprocal event causes cleanup', async () => {
     const observations: string[] = [];
@@ -71,28 +95,26 @@ describe('.cleanupPropagationEvent(sender, listener, target, event, reciprocal?)
     // reciprocal event cleanup scheduled in background using setImmediate,
     // so let the event loop run...
     await delay(100);
-    expect(observations).to.not.contain('listener');
-    expect(observations).to.contain('cleanup');
+    expect(observations).not.toContain('listener');
+    expect(observations).toContain('cleanup');
   });
   it('throws when cleanup propagation duplicated', () => {
     const sender = new EventEmitter();
-    const listener = (): void => null;
     const target = new EventEmitter();
     const event = 'close';
-    cleanupPropagationEvent(sender, event, listener, target);
-    expect(() => cleanupPropagationEvent(sender, event, listener, target)).to.throw(
-      /^Invalid operation; cleanup propagation already setup between/
-    );
+    cleanupPropagationEvent(sender, event, DummyListener, target);
+    expect(() =>
+      cleanupPropagationEvent(sender, event, DummyListener, target),
+    ).toThrow(/^Invalid operation; cleanup propagation already setup between/);
   });
   it('detects memory leak when cleanup target never performs cleanup', async () => {
     const observations: string[] = [];
     const sender = new EventEmitter();
-    const listener = (): void => null;
     const target = new EventEmitter();
     const event = 'close';
-    cleanupPropagationEvent(sender, event, listener, target, event);
+    cleanupPropagationEvent(sender, event, DummyListener, target, event);
     const target2 = new EventEmitter();
-    cleanupPropagationEvent(sender, event, listener, target2);
+    cleanupPropagationEvent(sender, event, DummyListener, target2);
     sender.once('error', (e: Error): void => {
       observations.push(e.message);
     });
@@ -100,35 +122,48 @@ describe('.cleanupPropagationEvent(sender, listener, target, event, reciprocal?)
     // reciprocal event cleanup scheduled in background using setImmediate,
     // so let the event loop run...
     await delay(100);
-    expect(observations.length).to.eql(1);
-    expect(observations[0]).to.match(/^Memory leak detected: EventEmitter/);
+    expect(observations.length).toEqual(1);
+    expect(observations[0]).toMatch(/^Memory leak detected: EventEmitter/);
   });
 });
 
 describe('.addCleanupTask(sender, listener, task)', () => {
   it('throws when sender not specified', () => {
-    expect(() => addCleanupTask(undefined, undefined, undefined)).to.throw('sender (object) is required');
+    expect(() =>
+      addCleanupTask(
+        undefined as unknown as EventEmitter,
+        undefined as unknown as string,
+        undefined as unknown as Task,
+      ),
+    ).toThrow('sender (object) is required');
   });
   it('throws when target not specified', () => {
-    expect(() => addCleanupTask(new EventEmitter(), undefined, undefined)).to.throw('target (object) is required');
+    expect(() =>
+      addCleanupTask(
+        new EventEmitter(),
+        undefined as unknown as string,
+        undefined as unknown as Task,
+      ),
+    ).toThrow('target (object) is required');
   });
   it('throws when task not specified', () => {
-    expect(() => addCleanupTask(new EventEmitter(), {}, undefined)).to.throw('task (func) is required');
+    expect(() =>
+      addCleanupTask(new EventEmitter(), {}, undefined as unknown as Task),
+    ).toThrow('task (func) is required');
   });
   it('throws when cleanup propagation not setup for sender', () => {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    expect(() => addCleanupTask(new EventEmitter(), {}, (): void => { })).to.throw(
-      /^Invalid operation: no cleanup propagation on EventEmitter/
-    );
+    expect(() =>
+      addCleanupTask(new EventEmitter(), {}, (): void => undefined),
+    ).toThrow(/^Invalid operation: no cleanup propagation on EventEmitter/);
   });
   it('throws when cleanup propagation not setup between sender and target', () => {
     const sender = new EventEmitter();
-    const listener = (): void => null;
     const target = new EventEmitter();
     const event = 'close';
-    cleanupPropagationEvent(sender, event, listener, target);
-    expect(() => addCleanupTask(sender, {}, () => null)).to.throw(
-      /^Invalid operation: no cleanup propagation setup between EventEmitter/
+    cleanupPropagationEvent(sender, event, DummyListener, target);
+    expect(() => addCleanupTask(sender, {}, () => null)).toThrow(
+      /^Invalid operation: no cleanup propagation setup between EventEmitter/,
     );
   });
 });
