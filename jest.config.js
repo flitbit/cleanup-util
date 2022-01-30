0a6812b1dae96f190b770232823120f3ce659bb8
Phillip Clark <phillip@flitbit.com>
2022-01-30 10:46:14 -0700

    refinement and docs

--- jest.config.js
+++ jest.config.js
@@ -9,14 +9,14 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
-  // coverageThreshold: {
-  //   global: {
-  //     // global thresholds
-  //     branches: 80,
-  //     functions: 80,
-  //     lines: 80,
-  //     statements: 80,
-  //   },
-  // },
+  coverageThreshold: {
+    global: {
+      // global thresholds
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   coverageReporters: ['lcov', 'text'],
 };
